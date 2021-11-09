c36ce6ba7149a2dbeaace8873bc3a8d2ef580071
BETTERFUTURE4 <50815519+BETTERFUTURE4@users.noreply.github.com>
2021-11-09 18:14:40 +0900

    Add files via upload

---
+++ Health_report/src/pages/Drugpage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h5  class="padding30">약 복용정보</h5>
+
+        <p> 그래프:
+            {{aveMedTime}}
+        </p>
+
+        <p>
+            
+            사람들은 평균적으로 {{ people_drug_time[0] }}AM, 
+            {{ people_drug_time[1] }}PM 에 약을 복용합니다.
+        </p>
+
+        <hr class="rotate">
+
+        <p> 그래프:
+            {{ aveMedTime[user_id] }}
+        </p>
+
+        <p>
+            {{user_id}}님은 평균적으로 {{ aveMedTime[user_id] }}시에 약을 복용합니다.
+        </p>
+    </div>
+    
+</template>
+
+<script>
+
+export default {
+    name: "Drugpage",
+    // 필요 변수 임포트
+    props : {
+        user_id : String,
+        aveMedTime : Object,
+        people_drug_time : Object
+    },
+    data(){
+        return {
+        }
+    },
+    methods : {
+    },
+}
+</script>
+
+<style>
+.rotate {
+    transform: rotate(-2deg);
+}
+</style>
---
+++ Health_report/src/pages/Mainpage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h5  class="padding30">{{user_id}}님의 생활패턴</h5>
+
+        <div>
+            그래프: {{act2[this.user_id]}}
+        </div>
+
+        <p>
+            <b v-if="user_most_participate.length > 0">{{user_most_participate.join(', ')}} 참여도</b>
+            가 가장 높아요!
+        </p>
+
+        <p v-if="user_most_participate.length == 0">
+            참여도가 높은 활동이 없어요!
+        </p>
+
+        <hr>
+
+        <h5>사람들의 생활패턴</h5>
+
+        그래프: {{act2}}
+
+    </div>
+    
+</template>
+
+<script>
+
+export default {
+    name: "Mainpage",
+    // 필요 변수 임포트
+    props : {
+        user_id : String,
+        act2 : Object,
+        user_most_participate : Object
+    },
+    data(){
+        return {
+
+        }
+    },
+    methods : {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
